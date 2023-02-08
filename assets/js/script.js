cf7f6ad2fed5e6142ae5987f174fd4555f182635
friaf <shovel.irena@gmail.com>
2023-02-08 15:25:57 +0000

    options done with commas

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,28 @@ document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 //async is chaining 2 then
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0]=== "options") {
+            optArray.push(entry[1]);
+        }
+    }
+
+    form.delete('options');
+    form.append('options', optArray.join());
+
+    return(form)
+
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
+    
+    //for (let entry of form.entries()) {
+       // console.log(entry)
+       // }
 
     const response = await fetch(API_URL, {
                         method: "POST",
@@ -40,9 +60,15 @@ function displayErrors(data) {
         results = `<div>Total Errors: <span>${data.total_errors}</span></div>`;
         for (let error of data.error_list) {
             results += `<div>At line <span class="line">${error.line}</span>`;
-            results += 
+            results += `column <span class='column'>${error.col}</span></div>`;
+            results += `<div class='error'>${error.error}</div>`;
         }
     }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 
 }
 
