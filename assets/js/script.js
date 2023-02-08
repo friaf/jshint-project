ee34c68c5e27d0285ce3361b8702ed87ceb03fa0
friaf <shovel.irena@gmail.com>
2023-02-08 14:02:28 +0000

    check results

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,53 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById('submit').addEventListener('click', e => postForm(e));
+//async is chaining 2 then
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById('checksform'));
+
+    const response = await fetch(API_URL, {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": API_KEY,
+                                 },
+                        body: form,
+                        })
+
+    const data = await response.json();
+
+    if(response.ok){
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+   // for (let entry of form.entries()) {
+       // console.log(entry);
+
+   // }
+}
+
+function displayErrors(data) {
+
+    let heading = `JSHint Results for ${data.file}`;
+
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported. Great job!</div>`;
+    } else {
+        results = `<div>Total Errors: <span>${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>`;
+            results += 
+        }
+    }
+
+}
 
 async function getStatus(e) {
 
     const queryString = `${API_URL}?api_key=${API_KEY}`;
-
+// await is used for promise (fetch, response, then)
     const response = await fetch(queryString);
 
     const data = await response.json();
@@ -28,6 +70,10 @@ function displayStatus(data) {
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
+
     resultsModal.show();
 
 }
+
+
+
