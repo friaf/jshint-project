52c54b4e64678d09b32860f38f33dd593b2cbd3f
friaf <shovel.irena@gmail.com>
2023-02-09 12:51:36 +0000

    displayException

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,6 +42,7 @@ async function postForm(e) {
     if(response.ok){
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
    // for (let entry of form.entries()) {
@@ -72,6 +73,8 @@ function displayErrors(data) {
 
 }
 
+
+
 async function getStatus(e) {
 
     const queryString = `${API_URL}?api_key=${API_KEY}`;
@@ -83,6 +86,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -93,6 +97,22 @@ function displayStatus(data) {
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
+}
+
+function displayException (data) {
+    
+    let heading = `An Exception Accurred`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
